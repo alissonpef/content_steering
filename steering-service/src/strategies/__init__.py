5e1206e5b82a33455083353ba98cb585178081b4
Alisson <alissonpef@gmail.com>
2026-02-27 13:35:18 -0300

    Remove D_UCB strategy import from the strategies module

--- steering-service/src/strategies/__init__.py
+++ steering-service/src/strategies/__init__.py
@@ -2,6 +2,5 @@ from .base import Selector
 from .epsilon_greedy import EpsilonGreedy
 from .simple import NoSteeringSelector, RandomSelector
 from .ucb import UCB1Selector
-from .d_ucb import D_UCB
 from .lin_ucb import LinUCBSelector
 from .oracle_best import OracleBestChoiceSelector
